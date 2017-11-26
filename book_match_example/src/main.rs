de40ebc35e55a0b17d4f5de8af4a8e168fb19af9
OwenMyers <owendalemyers@gmail.com>
2017-11-26 00:20:35 -0500

    Added a variables forlder for testing/learning about some of the simple variable types. Also got a couple more lines in the main.rs for learning about the match statement.

--- book_match_example/src/main.rs
+++ book_match_example/src/main.rs
@@ -35,5 +35,21 @@ fn main() {
     my_coin_value = value_in_cents(my_coin);
 
     println!("The value of my coin is: {}", my_coin_value);
+
+    println!("*****************************");
+    let some_u32_v: u32 = 4;
+    let some_u32_v2 = 5u32;
+    println!("some_u32_v: {}", some_u32_v);
+    println!("some_u32_v2: {}", some_u32_v2);
+
+    let some_u8_value = 0u8;
+    println!("some_u8_value: {}", some_u8_value);
+    match some_u8_value {
+        1 => println!("one"),
+        3 => println!("three"),
+        5 => println!("five"),
+        7 => println!("seven"),
+        _ => (),
+    }
 }
 
---
+++ variables/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+
+    // This is just a simple example of two differnt ways
+    // you can initilize some basic variables. Just different 
+    // sytax. Both valid.
+    let some_u32_v: u32 = 4;
+    let some_u32_v2 = 5u32;
+    println!("some_u32_v: {}", some_u32_v);
+    println!("some_u32_v2: {}", some_u32_v2);
+
+}
